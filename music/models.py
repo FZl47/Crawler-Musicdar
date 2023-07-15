fb39a4664ee7abb3b3bbbd95f9f922da40eeb69f
Fazel Momeni <wwwfazel88@gmail.com>
2023-07-16 00:13:36 +0330

    add blank on some fields model

--- music/models.py
+++ music/models.py
@@ -3,12 +3,12 @@ from django.db import models
 
 class Music(models.Model):
     title = models.CharField(max_length=200, null=True)
-    description = models.TextField(null=True)
-    picture = models.URLField(null=True)
-    singer = models.CharField(max_length=200, null=True)
-    categories = models.CharField(max_length=300, null=True, help_text='بر اساس | باید جدا شوند')
-    link_128 = models.URLField(null=True)
-    link_320 = models.URLField(null=True)
+    description = models.TextField(null=True, blank=True)
+    picture = models.URLField(null=True, blank=True)
+    singer = models.CharField(max_length=200, null=True, blank=True)
+    categories = models.CharField(max_length=300, null=True, blank=True, help_text='بر اساس | باید جدا شوند')
+    link_128 = models.URLField(null=True, blank=True)
+    link_320 = models.URLField(null=True, blank=True)
     url_page = models.URLField()
     is_out = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
