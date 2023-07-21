fcb4b62097cc5bcd33434889149dc5ac538121ef
Fazel Momeni <wwwfazel88@gmail.com>
2023-07-22 00:47:42 +0330

    add urls view public

--- public/urls.py
+++ public/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from . import views
 
-
 app_name = 'public'
 urlpatterns = [
-    path('',views.index)
+    path('', views.home, name='home'),
+    path('start-crawl', views.start_crawl, name='start_crawl'),
+    path('pause-crawl', views.pause_crawl, name='pause_crawl'),
+    path('continue-crawl', views.continue_crawl, name='continue_crawl'),
+    path('delete-crawl', views.delete_crawl, name='delete_crawl'),
+
+
+    path('tes', views.test, name='test'),
 ]
