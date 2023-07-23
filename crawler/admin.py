2193bac7177b16c2a1f7535dd36ec7098341342d
Fazel Momeni <wwwfazel88@gmail.com>
2023-07-23 22:37:01 +0330

    add some model in admin panel

--- crawler/admin.py
+++ crawler/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 from . import models
 
 admin.site.register(models.SiteModel)
-admin.site.register(models.MusicsModel)
+admin.site.register(models.CategoryModel)
 admin.site.register(models.MusicModel)
+admin.site.register(models.AddMusics)
+admin.site.register(models.AddMusicsCategoryState)
