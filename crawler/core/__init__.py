e5cf58770eb2d1372dd9356e638896f075132199
Fazel Momeni <wwwfazel88@gmail.com>
2023-07-23 22:45:56 +0330

    clean

--- crawler/core/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-    Files:
-        db   : save data in database musicdar.ir
-        file : manage files and upload in cloud storage
-        get  : crawl and get content from web pages
-        utils: usefully functions
-
-"""
