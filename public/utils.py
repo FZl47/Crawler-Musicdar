64afc899c646f73fa96a968dbd1e32d86038dd06
Fazel Momeni <wwwfazel88@gmail.com>
2023-07-23 22:38:04 +0330

    add log_write func

--- public/utils.py
+++ public/utils.py
@@ -1,4 +1,10 @@
+from django.conf import settings
 from public import tasks
+
+
+def log_write(log_obj, path=settings.LOG_CONFIG['path']):
+    with open(path,'a+',encoding='utf-8') as file:
+        file.write(log_obj.content)
 
 
 def send_email(subject: str, message: str = '', email_receiver: list = []):
